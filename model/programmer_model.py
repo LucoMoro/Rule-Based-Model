3de5b3cccdf34ad1c29d66b69d69780169a8f862
LucoMoro <morelli.2000luca@gmail.com>
2025-01-26 23:08:54 +0100

    feat: add programmer rules[5-10]

--- model/programmer_model.py
+++ model/programmer_model.py
@@ -6,32 +6,33 @@ class ProgrammerModel:
     def __init__(self, team: Team):
         self.team = team
 
+    #1 if is efficient,  0 if inefficient, 2 if there is no match
     def rule1(self, programmer: Programmer) -> int:
         if programmer.getSex() == "femmina":
             if "e" in programmer.getMbti():
                 return 1
             else:
-                return 0
+                return 2
         else:
-            return 0
+            return 2
 
     def rule2(self, programmer: Programmer) -> int:
         if "e" in programmer.getMbti():
             if "f" in programmer.getMbti():
                 return 1
             else:
-                return 0
+                return 2
         else:
-            return 0
+            return 2
 
     def rule3(self, programmer: Programmer) -> int:
         if "e" in programmer.getMbti():
             if "n" in programmer.getMbti():
                 return 1
             else:
-                return 0
+                return 2
         else:
-            return 0
+            return 2
 
     def rule4(self, programmer: Programmer) -> int:
         if "i" in programmer.getMbti():
@@ -45,6 +46,83 @@ class ProgrammerModel:
         else:
             return 2
 
-    #def rule5(self, programmer: Programmer) -> int:
-        #if programmer.getSex() == "maschio":
-            #test
+    def rule5(self, programmer: Programmer) -> int:
+        if programmer.getSex() == "maschio":
+            if "i" in programmer.getMbti():
+                if "t" in programmer.getMbti():
+                    if "j" in programmer.getMbti():
+                        return 0
+                    else:
+                        return 2
+                else:
+                    return 2
+            else:
+                return 2
+        else:
+            return 2
+
+    def rule6(self, programmer: Programmer) -> int:
+        if "e" in programmer.getMbti():
+            if "p" in programmer.getMbti():
+                return 0
+            else:
+                return 2
+        else:
+            return 2
+
+    def rule7(self, programmer: Programmer) -> int:
+        if programmer.getSex() == "maschio":
+            if "i" in programmer.getMbti():
+                if "f" in programmer.getMbti():
+                    return 0
+                else:
+                    return 2
+            else:
+                return 2
+        else:
+            return 2
+
+    def rule8(self, programmer: Programmer) -> int:
+        if programmer.getSex() == "maschio":
+            if "i" in programmer.getMbti():
+                if "p" in programmer.getMbti():
+                    return 0
+                else:
+                    return 2
+            else:
+                return 2
+        else:
+            return 2
+
+    def rule9(self, programmer: Programmer) -> int:
+        if programmer.getSex() == "femmina":
+            if "i" in programmer.getMbti():
+                if "n" in programmer.getMbti():
+                    if "t" in programmer.getMbti():
+                        return 1
+                    else:
+                        return 2
+                else:
+                    return 2
+            else:
+                return 2
+        else:
+            return 2
+
+    def rule10(self, programmer: Programmer) -> int:
+        if programmer.getSex() == "maschio":
+            if "e" in programmer.getMbti():
+                if "s" in programmer.getMbti():
+                    if "t" in programmer.getMbti():
+                        if "j" in programmer.getMbti():
+                            return 0
+                        else:
+                            return 2
+                    else:
+                        return 2
+                else:
+                    return 2
+            else:
+                return 2
+        else:
+            return 2
